fc88cfd2e734577d9c3168cdb699511e3afcf571
G. Rezniczek <g.rezniczek@gmail.com>
2021-09-16 10:15:04 +0200

    Fix disabled submit

--- surveyauth.js
+++ surveyauth.js
@@ -2,44 +2,54 @@ var RUBSurveyAuth = function() {
     return {
         disable: function(btn, disabled) {
             if (disabled) {
-                btn.prop('disabled', true)
-                btn.addClass('ui-button-disabled')
-                btn.addClass('ui-state-disabled')
+                btn.prop('disabled', true);
+                btn.addClass('ui-button-disabled');
+                btn.addClass('ui-state-disabled');
             }
             else {
-                btn.prop('disabled', false)
-                btn.removeClass('ui-button-disabled')
-                btn.removeClass('ui-state-disabled')
+                btn.prop('disabled', false);
+                btn.removeClass('ui-button-disabled');
+                btn.removeClass('ui-state-disabled');
             }
         },
         setup: function(prefix) {
             // Find ui elements.
-            var uinput = $('#' + prefix + '-username')
-            var pinput = $('#' + prefix + '-password')
-            var submit = $('#' + prefix + '-submit')
-            var failmsg = $('#' + prefix + 'failmsg')
+            var uinput = $('#' + prefix + '-username');
+            var pinput = $('#' + prefix + '-password');
+            var submit = $('#' + prefix + '-submit');
+            var failmsg = $('#' + prefix + 'failmsg');
             var onChange = function() {
                 var username = uinput.val();
                 var pwd = pinput.val();
-                var disabled = username.length == 0 || pwd.length == 0
-                RUBSurveyAuth.disable(submit, disabled)
-                failmsg.hide()
+                var disabled = username.length == 0 || pwd.length == 0;
+                RUBSurveyAuth.disable(submit, disabled);
+                failmsg.hide();
             }
-            uinput.on('keypress', onChange)
-            pinput.on('keypress', onChange)
+            var onEnter = function() {
+                if (submit.prop('disabled')) {
+                    var username = uinput.val();
+                    var pwd = pinput.val();
+                    if (username != '' && pwd != '') {
+                        RUBSurveyAuth.disable(submit, false);
+                    }
+                }
+            }
+            submit.parent().on('mouseenter', onEnter);
+            uinput.on('keypress paste blur', onChange);
+            pinput.on('keypress paste blur', onChange);
             onChange()
         },
         submit: function(prefix) {
             // Find ui elements.
             var form = $('#' + prefix + '-form');
-            var uinput = $('#' + prefix + '-username')
-            var pinput = $('#' + prefix + '-password')
+            var uinput = $('#' + prefix + '-username');
+            var pinput = $('#' + prefix + '-password');
             var username = uinput.val();
             var pwd = pinput.val();
             if (username == '' || pwd == '') {
                 return false;
             }
-            form.submit()
+            form.submit();
         }
     }
 }();
